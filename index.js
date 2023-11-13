6a5538e71aed9805792e90ae43c7ff0dcebaae58
Preston Fitzgerald <prestonfitz@gmail.com>
2023-11-13 08:54:29 -0700

    Creating a backup

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ let path = require("path");
 // make app an express object
 let app = express();
 
-app.use(express.static(path.join(__dirname, '/hw7')));
+app.use(express.static(path.join(__dirname, '/html')));
 app.listen(3000, () => console.log('Server is Running'));
 
 
 app.get("/", (req, res) => {      
-    res.sendFile(path.join(__dirname + '/hw7/hw7.html'));
-});   
+    res.sendFile(path.join(__dirname + '/html/index.html'));
+});
+
+app.get("/hw7", (req, res) => {
+    res.sendFile(path.join(__dirname + '/html/hw7/hw7.html'))
+});
