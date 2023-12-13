134b255dfd6e7d4d5eaac58518a182a559c50e07
Preston Fitzgerald <prestonfitz@gmail.com>
2023-12-13 09:33:58 -0700

    Hopefully this works

--- index.js
+++ index.js
@@ -252,32 +252,32 @@ app.post("/submitResponses", (req, res) => {
   const id = req.body.id;
   const question1 = req.body.answer1;
 
-  knex("CourseResponses").insert({
-    UserID: req.session.userid,
-    ProductID: req.body.id,
-    QuestionNum: 1,
-    Response: req.body.answer1,
-    ResponseDate: getTodayDate()
- }).then(account => {});
+//   knex("CourseResponses").insert({
+//     UserID: req.session.userid,
+//     ProductID: req.body.id,
+//     QuestionNum: 1,
+//     Response: req.body.answer1,
+//     ResponseDate: getTodayDate()
+//  }).then(account => {});
 
-  knex("CourseResponses").insert({
-    UserID: req.session.userid,
-    ProductID: req.body.id,
-    QuestionNum: 2,
-    Response: req.body.answer2,
-    ResponseDate: getTodayDate()
- }).then(account => {});
+//   knex("CourseResponses").insert({
+//     UserID: req.session.userid,
+//     ProductID: req.body.id,
+//     QuestionNum: 2,
+//     Response: req.body.answer2,
+//     ResponseDate: getTodayDate()
+//  }).then(account => {});
 
-  knex("CourseResponses").insert({
-    UserID: req.session.userid,
-    ProductID: req.body.id,
-    QuestionNum: 3,
-    Response: req.body.answer3,
-    ResponseDate: getTodayDate()
- }).then(account => {});
+//   knex("CourseResponses").insert({
+//     UserID: req.session.userid,
+//     ProductID: req.body.id,
+//     QuestionNum: 3,
+//     Response: req.body.answer3,
+//     ResponseDate: getTodayDate()
+//  }).then(account => {});
 
 // here I need to add a value for user id to pass it if we want to add it to the database
-  res.render('submitResponses', {name: name, id: id, question1: question1});
+  return res.render('submitResponses', {name: name, id: id, question1: question1});
 })
 
 // about
